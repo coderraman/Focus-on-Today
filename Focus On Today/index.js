399cf1eb7350093693e0b34a440a786ea145fd94
coderraman <129317923+coderraman@users.noreply.github.com>
2024-06-19 15:31:41 +0530

    Add files via upload

---
+++ Focus On Today/index.js
@@ -0,0 +1,9 @@
+let input_fill = document.querySelectorAll(".input-fill");
+let circles = document.querySelectorAll(".circle");
+circles.forEach((circle)=>{
+    circle.addEventListener("click",(e)=>{
+        circle.style.backgroundColor = "green"
+    })
+})
+
+
